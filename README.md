840027947df7fe41f5195a3f63b2836e8d2fa97e
Vlad <vlad@difogic.com>
2015-09-02 15:14:45 +0300

    ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # etcd-simple-config
 Simple config management with Etcd
+
+See [example.js] (https://github.com/vlad-x/etcd-simple-config/blob/master/example.js)
+
+## API
+
+### etcdConfig.bind(prefix, defaultConfig, changeCallback)
+Bind etcd path `prefix`, providing the default config `defaultConfig`, get the current config and start watching for updates with `changeCallback`
+
+```js
+var etcdConfig = new EtcdSimpleConfig('127.0.0.1', 4001);
+
+var config = etcdConfig.bind(prefix, defaultConfig, function(key, change){
+	console.log('Config changed', change, config);
+});
+```
+
+### config = etcdConfig.get(prefix)
+get config
+
+### etcdConfig.set(prefix, obj)
+add or update values
--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 var EtcdSimpleConfig = require('./index');
 
-var etcdConfig = new EtcdSimpleConfig();
+var etcdConfig = new EtcdSimpleConfig('127.0.0.1', 4001);
 
 var defaultConfig = {
 	title: 'Hello world',
@@ -15,8 +15,16 @@ var defaultConfig = {
 	}
 };
 
-var config = etcdConfig.bind('prefix/my-awesome-app', defaultConfig, function(key, change){
+var prefix = 'prefix/my-awesome-app';
+
+// Bind a etcd path, providing the default config, get the current config and start watching for updates
+
+var config = etcdConfig.bind(prefix, defaultConfig, function(key, change){
 	console.log('Config changed', change, config);
 });
 
 console.log('config', config);
+
+// add or update values
+etcdConfig.set(prefix, { another_value: 'Another value' });
+etcdConfig.set(prefix, { max_requests: 3 });
